276f3eea7c3cb9b2463027f70fb6f4430a652f35
Ian <12iandpark@gmail.com>
2022-08-25 02:28:03 -0700

    Updating std_dev Data sort

--- std_dev.py
+++ std_dev.py
@@ -7,16 +7,16 @@ import numpy.fft as fft
 
 model_data = pd.read_pickle("train_data.pkl")
 model_data["Day_of_Year"] = model_data.index.day_of_year 
-# running_avg = 0.0
-# for i in range(1,366):
-#     Day = model_data[(model_data["Day_of_Year"] == i)]
-#     running_avg += np.std(Day["Target_Value"])
+running_avg = 0.0
+for i in range(1,366):
+    Day = model_data[(model_data["Day_of_Year"] == i)]
+    running_avg += np.std(Day["Target_Value"])
     
-# #print(model_data["TMAX"].corr(model_data["MONTH"]))
-# print(running_avg/365)
+#print(model_data["TMAX"].corr(model_data["MONTH"]))
+print(running_avg/365)
 
-c = fft.rfft(model_data["Target_Value"].values)
+# c = fft.rfft(model_data["Target_Value"].values)
 
-plt.plot(c)
-plt.xlim(left = -100, right = 500)
-plt.show()
+# plt.plot(c)
+# plt.xlim(left = -100, right = 500)
+# plt.show()
